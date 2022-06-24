4c26745902befc778ec9f20e70976bd25dbe6932
Trọng Vương <67507755+trongbaby0108@users.noreply.github.com>
2022-06-25 06:08:37 +0700

    upload

---
+++ SanPham/lib/src/main/java/com/example/lib/Interface/IChuyenData.java
@@ -0,0 +1,9 @@
+package com.example.lib.Interface;
+
+import com.example.lib.Model.Invoice;
+import com.example.lib.Model.product;
+
+public interface IChuyenData {
+    public void ChuyenData(product product);
+    public void PassDetailBill(Invoice bill);
+}
